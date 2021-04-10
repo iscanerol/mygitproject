1678912c29eedb80c3fd65ac364bda810bd2f468
iscanerol <iscanerol0@gmail.com>
2021-04-10 16:02:16 +0300

    ikinci orsem

--- src/mygitpackage/MygitClass.java
+++ src/mygitpackage/MygitClass.java
@@ -5,8 +5,8 @@ public class MygitClass {
 	public static void main(String[] args) {
 
 		System.out.println("Version 1");
-		System.out.println("my second commit");
-		System.out.println("my thirth commit");
+		System.out.println("my second committt");
+		System.out.println("my thirth committttt");
 	}
 
 }
