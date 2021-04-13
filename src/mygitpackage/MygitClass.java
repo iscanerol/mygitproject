6af97047e995d171c15e2a2da9592d52b3c66fa6
iscanerol <iscanerol0@gmail.com>
2021-04-13 14:47:33 +0300

    slm

--- src/mygitpackage/MygitClass.java
+++ src/mygitpackage/MygitClass.java
@@ -12,7 +12,13 @@ public class MygitClass {
 		System.out.println("my 4 th commit");
 		System.out.println("uzak sunucudaki kodlarim 22");
 		System.out.println("uzak sunucudaki kodlarim 22 ali");
-		System.out.println("uzak sunucudaki kodlarim 22 veli");
+		 System.out.println("uzak sunucudaki kodlarim 22 veli");
+		 
+
+		 System.out.println("uzak sunucudaki kodlarim 22");
+			System.out.println("uzak sunucudaki kodlarim 22 ali");
+			System.out.println("uzak sunucudaki kodlarim 22 veli");
+		 
 	}
 
 }
