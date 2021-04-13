ce4d31f4f6d7d241c35e2e7d89b8bec9942a3aca
iscanerol <67128973+iscanerol@users.noreply.github.com>
2021-04-13 14:46:03 +0300

    Update MygitClass.java

--- src/mygitpackage/MygitClass.java
+++ src/mygitpackage/MygitClass.java
@@ -13,6 +13,9 @@ public class MygitClass {
 		System.out.println("uzak sunucudaki kodlarim 22");
 		System.out.println("uzak sunucudaki kodlarim 22 ali");
 		System.out.println("uzak sunucudaki kodlarim 22 veli");
+		System.out.println("uzak sunucudaki kodlarimmm 22");
+		System.out.println("uzak sunucudaki kodlariiim 22 ali");
+		System.out.println("uzak sunucudaki kodlarim 22 velioo");
 	}
 
 }
